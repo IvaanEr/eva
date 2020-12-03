36b3aa228a3bdefd27ae6aa842087c81c772cdbd
Ivan Ernandorena <ivan_ernandorena@hotmail.com>
2020-12-03 11:36:38 -0300

    Almost there

--- 2-install.sh
+++ 2-install.sh
@@ -6,9 +6,9 @@ print "Installing and configuring NeoVim"
 brew install neovim
 mkdir -p ~/.config/nvim/
 
-ln -s ./neovim/init.vim ~/.config/nvim/init.vim
-ln -s ./neovim/coc-settings.json ~/.config/nvim/coc-settings.json
-ln -s ./neovim/.vimrc ~/.vimrc
+ln  ./neovim/init.vim home/$USER/.config/nvim
+ln  ./neovim/coc-settings.json home/$USER/.config/nvim
+ln  ./neovim/.vimrc home/$USER
 print "done"
 
 print "Installing powerlevel10k"
@@ -20,67 +20,41 @@ nvim ~/.zshrc
 print "done"
 
 print "Installing kitty Terminal"
-brew tap homebrew/cask-fonts
-brew cask install font-victor-mono
-
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin \
-    launch=n
+sudo apt-get install -y kitty
 
 mkdir -p ~/.config/kitty/
-ln -s ./kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -s ./kitty/embark.conf ~/.config/kitty/embark.conf
-
+ln  ./kitty/kitty.conf ~/.config/kitty
+ln  ./kitty/embark.conf ~/.config/kitty
 sudo update-alternatives --config x-terminal-emulator
 print "done"
 
-#print "Installing HomeBrew"
-#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-#sudo apt-get install build-essential
+print "Install cool fonts"
+sudo apt-get install -y fonts-firacode
+print "done"
 
-#print "Installing and configuring NeoVim"
-#brew install neovim
-#mkdir -p ~/.config/nvim/
+print "Installing Google Chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+print "done"
 
-#ln -s ./neovim/init.vim ~/.config/nvim/init.vim
-#ln -s ./neovim/coc-settings.json ~/.config/nvim/coc-settings.json
-#ln -s ./neovim/.vimrc ~/.vimrc
-#print "done"
+print "Installing asdf"
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+  cd ~/.asdf
+  git checkout "$(git describe --abbrev=0 --tags)"
 
-#print "Installing kitty Terminal"
-#brew tap homebrew/cask-fonts
-#brew cask install font-victor-mono
+  print "Installing plugins"
+  sudo apt-get -y install build-essential autoconf m4 libncurses5-dev \
+  libwxgtk3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev \
+  unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk unzip \
+    dirmngr gpg
 
-#curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin \
-#    launch=n
+  asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+  asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+  asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+print "done"
 
-#mkdir -p ~/.config/kitty/
-#ln -s ./kitty/kitty.conf ~/.config/kitty/kitty.conf
-#ln -s ./kitty/embark.conf ~/.config/kitty/embark.conf
-
-#sudo update-alternatives --config x-terminal-emulator
-#print "done"
-
-#print "Installing Google Chrome"
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#sudo dpkg -i google-chrome-stable_current_amd64.deb
-#rm google-chrome-stable_current_amd64.deb
-#print "done"
-
-#print "Installing asdf"
-#  git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-#  cd ~/.asdf
-#  git checkout "$(git describe --abbrev=0 --tags)"
-
-#  print "Installing plugins"
-#  sudo apt-get -y install build-essential autoconf m4 libncurses5-dev \
-#  libwxgtk3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev \
-#  unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk unzip \
-#    dirmngr gpg
-
-#  asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
-#  asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-#  asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-#  bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
 ##git clone git@github.com:elixir-lsp/elixir-ls.git
 ##cd elixir-ls
 ##mix deps.get
